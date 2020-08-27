59b0064272e72ab62eab205d6c63e7e60495b097
Julius Schmidt <js@spraken.de>
2020-08-27 15:52:59 +0200

    Apply ds300/postinstall-postinstall #7

--- run.js
+++ run.js
@@ -1,24 +1,24 @@
-const fs = require('fs')
-const cwd = require('process').cwd()
-const path = require('path')
-const exec = require('child_process').execSync
+const fs = require("fs");
+const cwd = require("process").cwd();
+const path = require("path");
+const exec = require("child_process").exec;
 
-const appPath = path.normalize(cwd.slice(0, cwd.lastIndexOf('node_modules')))
+const appPath = path.normalize(cwd.slice(0, cwd.lastIndexOf("node_modules")));
 
-const packageJsonPath = path.join(appPath, 'package.json')
+const packageJsonPath = path.join(appPath, "package.json");
 
 if (fs.existsSync(packageJsonPath)) {
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath))
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 
   if (packageJson.scripts && packageJson.scripts.postinstall) {
-    const pkgManager = shouldUseYarn() ? 'yarn' : 'npm';
-    exec(`${pkgManager} run postinstall`, {cwd: appPath})
+    const pkgManager = shouldUseYarn() ? "yarn" : "npm";
+    exec(`${pkgManager} run postinstall`, { cwd: appPath });
   }
 }
 
 function shouldUseYarn() {
   try {
-    exec('yarnpkg --version', { stdio: 'ignore' });
+    exec("yarnpkg --version", { stdio: "ignore" });
     return true;
   } catch (e) {
     return false;
